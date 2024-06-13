debc10243f4bf3b479d672bf0c8c48b539c1d856
Vamshi Krishna S <107762231+VamshiKrsna@users.noreply.github.com>
2024-06-13 22:36:20 +0530

    updated moviesummarizer.py

--- moviesummarizer.py
+++ moviesummarizer.py
@@ -15,25 +15,31 @@ examples = [
 ]
 
 
-movie = "Requiem for a dream" # 🏠💊📦😵💸👀💔😔😩
+# movie = "Requiem for a dream" # 🏠💊📦😵💸👀💔😔😩
 
 client = Groq()
 
-response = client.chat.completions.create(
-  model="llama3-8b-8192",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt.format(movie = examples[0]["input"])},
-    {"role":"assistant","content":prompt.format(movie=examples[1]["output"])},
-    {"role": "user", "content": prompt.format(movie=examples[1]["input"])},
-    {"role": "assistant", "content": examples[1]["output"]},
-    {"role": "user", "content": prompt.format(movie=movie)},
-  ]
-)
+def get_movie_emoji_summary(movie : str) -> str:
+  response = client.chat.completions.create(
+    model="llama3-8b-8192",
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": prompt.format(movie = examples[0]["input"])},
+      {"role":"assistant","content":prompt.format(movie=examples[1]["output"])},
+      {"role": "user", "content": prompt.format(movie=examples[1]["input"])},
+      {"role": "assistant", "content": examples[1]["output"]},
+      {"role": "user", "content": prompt.format(movie=movie)},
+    ]
+  )
+  return response.choices[0].message.content
 
 # print(type(response))
 # print(response['choices'][0]['message']['content'])
 
-print(f"Your Prompt : {movie}")
+# print(f"Your Prompt : {movie}")
+#
+# print(response.choices[0].message.content)
 
-print(response.choices[0].message.content)
+print(get_movie_emoji_summary("Avatar"))
+print(get_movie_emoji_summary("Harry Potter"))
+print(get_movie_emoji_summary("Oppenheimer"))
